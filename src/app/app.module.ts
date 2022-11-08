3a619fc1fd70cb62408ac11c25a0c431d20284c5
Amirtha <65668646+amirthalakshmi22@users.noreply.github.com>
2022-11-08 18:03:10 +0530

    update final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RoomBookingComponent } from './modules/room-booking/room-booking.component';
+//import { RoomBookingComponent } from './modules/booking/components/room-booking.component';
 import { MembershipComponent } from './modules/shared/components/membership/membership.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoomBookingComponent,
-    MembershipComponent
-  ],
+   
+    MembershipComponent,
+         ],
   imports: [
     BrowserModule,
     AppRoutingModule
