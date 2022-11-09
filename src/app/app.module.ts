179821bc227db2608882fe5b4aa0721625f57f9f
Aashi Barodiya <Aashi.Barodiya@BRILLIO.COM>
2022-11-09 11:00:22 +0530

    header-add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './modules/core/components/footer/footer.component';
+import { HeaderComponent } from './modules/core/components/header/header.component';
+import { HomeComponent } from './modules/core/components/home/home.component';
+
 //import { RoomBookingComponent } from './modules/booking/components/room-booking.component';
 import { MembershipComponent } from './modules/shared/components/membership/membership.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-   
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
     MembershipComponent,
          ],
   imports: [
--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,25 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ComponentsComponent } from './components/components.component';
+
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { UsersComponent } from './users/users.component';
-import { RoomBookingComponent } from './room-booking/room-booking.component';
+
 import { HomeComponent } from './components/home/home.component';
 
 
 
 @NgModule({
   declarations: [
-    ComponentsComponent,
+   
     HeaderComponent,
     FooterComponent,
-    UsersComponent,
-    RoomBookingComponent,
+  
     HomeComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    HeaderComponent,
+    FooterComponent,
+   
+    HomeComponent,
+    CoreModule
   ]
 })
 export class CoreModule { }
