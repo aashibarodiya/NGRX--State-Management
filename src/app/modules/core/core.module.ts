fca528054cfafeb9ff1887e59258b96e12ac62c4
Anbumani T <tanbumani1402@gmail.com>
2022-11-09 17:36:54 +0530

    changed core modules

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -4,10 +4,6 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 0a15bbbc21ebd52f02639fa98e8d303df8518bf8
 import { HomeComponent } from './components/home/home.component';
 import { MembershipComponent } from '../shared/components/membership/membership.component';
 import { SharedModule } from '../shared/shared.module';
@@ -19,13 +15,6 @@ import { UsersModule } from '../users/users.module';
 
 @NgModule({
   declarations: [
-<<<<<<< HEAD
- 
-    HeaderComponent,
-    FooterComponent,
-    
-    HomeComponent
-=======
   
     HeaderComponent,
     FooterComponent,
@@ -33,7 +22,6 @@ import { UsersModule } from '../users/users.module';
     MembershipComponent,
     LoginComponent,
     RegisterComponent
->>>>>>> 0a15bbbc21ebd52f02639fa98e8d303df8518bf8
   ],
   imports: [
     CommonModule,
