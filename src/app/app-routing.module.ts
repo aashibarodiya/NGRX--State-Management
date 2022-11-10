5a9c4c8aae9b2563cc068c0c06746472ebecf2e2
Anbumani T <tanbumani1402@gmail.com>
2022-11-10 12:19:54 +0530

    deleted import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GalleryComponent } from './modules/booking/components/gallery/gallery.component';
 
-import { GalleryComponent } from './modules/booking/components/gallery/gallery.component';
+
 
 
 
